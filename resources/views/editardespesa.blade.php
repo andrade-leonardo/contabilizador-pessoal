74bcfc6db1cf3fa8bc5bf823395850e72de88958
Leonardo-de-Andrade <leonardo-de-andrade@hotmail.com>
2021-06-18 12:50:46 -0300

    correcao de problemas com id

--- resources/views/editardespesa.blade.php
+++ resources/views/editardespesa.blade.php
@@ -4,15 +4,15 @@
 
 <div class="card border">
     <div class="card-body">
-        <form action="/despesas/{{$res->id}}" method="POST">
+        <form action="/despesas/{{$des->id}}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="tipoReceita">Tipo de Receita</label>
-                <input type="text" class="form-control" name="tipoReceita"
-                       id="tipoReceita" placeholder="Digite o tipo de receita" required>
-                <label for="valorReceita">Valor</label>
-                 <input type="text" class="form-control" name="valorReceita"
-                     id="valorReceita" placeholder="Digite valor da receita" required>
+                <label for="tipoDespesa">Tipo de Despesa</label>
+                <input type="text" class="form-control" name="tipoDespesa"
+                       id="tipoDespesa" placeholder="Digite o tipo de despesa" required>
+                <label for="valorDespesa">Valor</label>
+                 <input type="text" class="form-control" name="valorDespesa"
+                     id="valorDespesa" placeholder="Digite valor da despesa" required>
             </div>
             <button type="submit" class="btn btn-primary btn-sm" style="margin-top: 10px;">Salvar</button>
             <button type="cancel" class="btn btn-danger btn-sm" style="margin-top: 10px;">Cancel</button>
